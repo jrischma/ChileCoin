343b7d8c28bafa336428f4af4a47678c648facf7
jrischma <jrischma@gmail.com>
2018-01-25 03:34:17 -0300

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "ChileCoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff00018ffb8727029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101be213bfd2ac76dde7792480fe239dab38eec33ab5bfd2d5d06dfddd23aeeec07";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
